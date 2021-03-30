a1c54c9ae08d08f71df7d814e6001ddc1fabc15e
Krista <kb9286@gmail.com>
2021-03-30 16:08:37 -0500

    pivoted to webpack

--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
+const path = require('path')
+
 const express = require("express")
 
+if(process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line global-require
+  require('dotenv').config({ path: './.env' });
+}
+
 const app = express()
+const {PORT} = process.env
+const {HOST} = process.env
 
 app.use(express.json())
-app.use(express.urlendcoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.use (express.static(__dirname + "../dist"))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
   res.status(200).end()
 })
 
 app.listen(PORT, (err) => {
-  if (err) { return console.error(err) }
-  else { return `listening on port ${process.env.PORT}`}
-})
+  if (err) { console.error(err); }
+  else { console.log(`listening at http://${HOST}:${PORT}`); }
+});
