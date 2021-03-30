76bb9c22e8fffe57b224ec422f9cedafb964ae34
Krista <kb9286@gmail.com>
2021-03-30 13:38:31 -0500

    added server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlendcoded({ extended: true }))
+
+app.use (express.static(__dirname + "../dist"))
+
+app.get('/', (req, res) => {
+  res.status(200).end()
+})
+
+app.listen(PORT, (err) => {
+  if (err) { return console.error(err) }
+  else { return `listening on port ${process.env.PORT}`}
+})
