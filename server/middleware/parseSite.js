f30a81b1e28a5404dff31a2d154874a5ad6fa50b
Krista <kb9286@gmail.com>
2021-03-31 22:56:03 -0500

    source code can now be filtered

---
+++ server/middleware/parseSite.js
@@ -0,0 +1,10 @@
+const html = 'I altered Jibberboy2000s answer to include several <BR /> tag formats, remove everything inside <SCRIPT> and <STYLE> tags, <p>format the resulting HTML by removing multiple line breaks and spaces and convert some HTML-encoded code into normal. After some testing it appears that you can convert most of full web pages into simple text where page title and content are retained.<h3>In the simple example</h3></p>,<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><!--comment--><head><title>This is my title</title><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><style>body {margin-top: 15px;}a { color: #D80C1F; font-weight:bold; text-decoration:none; }</style></head><body><center><h1>This string has</h2> <i>html</i> code i want to <b>remove</b><br>In this line <a href="http://www.bbc.co.uk">BBC</a> with link is mentioned.<br>Now back to &quot;normal text&quot; and stuff using &lt;html encoding gt<center></body></html>'
+
+const { filterSite } = require('./filterSite.js')
+const { checkWhiteList } = require('./checkWhiteList.js')
+
+exports.parseSite = (source) => {
+  const filtered = filterSite(source)
+
+  return filtered.split("<").filter((tag) => checkWhiteList(tag)).map((tag) => checkWhiteList(tag)).join("")
+}
